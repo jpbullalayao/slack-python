1684bf1986277794d34fcf317eab822eaf032aad
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-12 17:10:28 -0700

    Add support for Creating a channel

--- slack/api_resources/channel.py
+++ slack/api_resources/channel.py
@@ -1,2 +1,9 @@
-class Channel:
-    pass
+from slack.api_resources.abstract.createable_api_resource import CreateableAPIResource  # NOQA
+
+
+class Channel(CreateableAPIResource):
+    OBJECT_NAME = "channels"
+    
+    @classmethod
+    def post_url(cls):
+        return cls.class_url() + 'create'
