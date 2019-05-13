83d0f74fcf8246c8790f419020582ade8bec9151
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-12 17:28:31 -0700

    Add support for creating private channels

--- slack/api_resources/group.py
+++ slack/api_resources/group.py
@@ -1,2 +1,9 @@
-class Group:
-    pass
+from slack.api_resources.abstract.createable_api_resource import CreateableAPIResource  # NOQA
+
+
+class Group(CreateableAPIResource):
+    OBJECT_NAME = "groups"
+
+    @classmethod
+    def post_url(cls):
+        return cls.class_url() + 'create'
