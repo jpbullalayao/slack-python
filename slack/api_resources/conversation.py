4c0ac0e340b47bcba7e27177009409c32ed40d38
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-12 17:24:09 -0700

    Add support for adding Conversations

--- slack/api_resources/conversation.py
+++ slack/api_resources/conversation.py
@@ -1,2 +1,9 @@
-class Conversation:
-    pass
+from slack.api_resources.abstract.createable_api_resource import CreateableAPIResource  # NOQA
+
+
+class Conversation(CreateableAPIResource):
+    OBJECT_NAME = "conversations"
+
+    @classmethod
+    def post_url(cls):
+        return cls.class_url() + 'create'
